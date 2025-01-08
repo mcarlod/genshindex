edc89169a8113a2e2a711d0783e6f17b5a92440d
diemy006 <diemy006@mymail.unisa.edu.au>
2025-01-08 21:05:30 +1030

    Started implementation of character preview cards.

---
+++ src/components/CharactersPreview.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+
+const CharactersPreview = (props) => {
+  const [card, setCard] = useState([])
+  const [icon, setIcon] = useState([])
+  const [images, setImage] = useState([])
+
+  useEffect(() => {
+    //calling apis
+  })
+}
